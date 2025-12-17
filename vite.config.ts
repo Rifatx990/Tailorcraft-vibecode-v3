f0b57edf9e880a42b7f6d9b42c7ce3fb6f131590
Rifatx990 <158224402+Rifatx990@users.noreply.github.com>
2025-12-17 09:43:59 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
     return {
       server: {
         port: 3000,
-        host: 'tailorcraft-vibecode-v3.onrender.com',
+        host: '0.0.0.0',
       },
       plugins: [react()],
       define: {
